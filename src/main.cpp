29c71c5dd0eaad8e8c909483cbf75aac0fd87cf1
Nguyen Van Mung <vanmungxuanduong61@gmail.com>
2023-12-01 15:45:38 +0700

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,116 @@
 #include <WiFi.h>
-
-const char* ssid = "Sv 2022";
-const char* password = "@sv22022";
-const char *serverIp = "localhost";
+#include <ctime>
+#include "time.h"
+const char *ssid = "Sv 2022";
+const char *password = "@sv22022";
+const char *serverIp = "192.168.1.83";
 const int serverPort = 8080;
+String volStr = "";
+const char *ntpServer = "pool.ntp.org";
+// Variable to save request epoch time
+unsigned long epochTime;
+int volt;
 
-const int pirPin = 13; // Replace with the actual pin your PIR sensor is connected to
+int delayTime = 10;
+
+int countMax = 1000 / delayTime;
+int count = 0;
+// Function get time
+unsigned long getTime();
+const int pirPin = 34; // Replace with the actual pin your PIR sensor is connected to
+
 bool checkForMotion();
 void sendDataToServer();
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
+  configTime(0, 0, ntpServer);
 
   pinMode(pirPin, INPUT);
 
   // Your PIR sensor logic goes here
 }
 
-void loop() {
+void loop()
+{
   // When motion is detected, send data to the server
-  if (checkForMotion()) {
+
+  // Wait for 5 seconds to avoid sending data too frequently
+
+  count++;
+
+  volt = analogRead(pirPin);
+
+  volStr += (count < countMax) ? String(volt) + '+' : String(volt);
+
+  delay(delayTime);
+
+  if (count == countMax)
+  {
     sendDataToServer();
-    delay(5000); // Wait for 5 seconds to avoid sending data too frequently
+    epochTime = getTime();
+    Serial.println("Detected!");
+    // String dectected = "POST / HTTP/1.1 esp-id=0&vol=" + volStr + "&time=" + String(epochTime);
+    // Serial.println(dectected);
+
+    // reset count and volstr
+    count = 0;
+    volStr = "";
   }
 }
 
-bool checkForMotion() {
+unsigned long getTime()
+{
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return (0);
+  }
+  time(&now);
+  return now;
+}
+bool checkForMotion()
+{
   // Replace this with your PIR sensor logic
   // Return true if motion is detected, false otherwise
   return digitalRead(pirPin) == HIGH;
 }
 
-void sendDataToServer() {
+void sendDataToServer()
+{
   WiFiClient client;
 
   // Connect to the server
-  if (client.connect(serverIp, serverPort)) {
+  if (client.connect(serverIp, serverPort))
+  {
     Serial.println("Connected to server");
-
     // Construct the data payload
-    String postData = "device id:0\nvoltage:5\ntime:" + String(millis());
-
-    Serial.println("Sending data to server:\n" + postData);
+    epochTime = getTime();
+    volt = analogRead(pirPin);
+    String postData = "POST / HTTP/1.1 esp-id=0&vol=" + volStr + "&time=" + String(epochTime);
+    Serial.println(postData);
+    // Serial.println("Sending data to server:\n" + postData);
 
     // Send the data to the server
-    client.print("POST / HTTP/1.1\r\n");
-    client.print("Host: " + String(serverIp) + "\r\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\r\n");
-    client.print("Content-Length: " + String(postData.length()) + "\r\n");
-    client.print("\r\n");
     client.print(postData);
 
     // Wait for the response
-    while (client.connected()) {
-      if (client.available()) {
+    while (client.connected())
+    {
+      if (client.available())
+      {
         String line = client.readStringUntil('\r');
         Serial.println(line);
       }
@@ -69,7 +118,9 @@ void sendDataToServer() {
 
     // Close the connection
     client.stop();
-  } else {
+  }
+  else
+  {
     Serial.println("Unable to connect to server");
   }
 }
